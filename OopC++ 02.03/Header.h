b320c0ca12eccdae553a460385c90830f18c69d4
考え-Sueta <vityayuzbekov602@gmail.com>
2023-03-02 17:39:09 +0200

    .

---
+++ OopC++ 02.03/Header.h
@@ -0,0 +1,16 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <ctime>
+#include <stdlib.h>
+#include <iomanip>
+#include <conio.h>
+#include <string>
+#include <string.h>
+#include <ctype.h>
+#include <string>
+#include<vector>
+#include<functional>
+#include <algorithm>
+#include <array>
+using namespace std;
